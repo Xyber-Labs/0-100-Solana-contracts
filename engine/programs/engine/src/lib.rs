1e604e6db377f4700fe468ef7bc78ca0a16d5279
xykeeper <xykeeper@xyber.inc>
2025-10-01 23:29:34 +0500

    Polish

--- engine/programs/engine/src/lib.rs
+++ engine/programs/engine/src/lib.rs
@@ -135,9 +135,8 @@ pub mod engine {
         
         // Check if funding period has ended
         let current_time = Clock::get()?.unix_timestamp;
-        // TODO: fix that in tests @keeper @wotori
-        // require!(current_time >= st.funding_period_end, ErrorCode::FundingPeriodNotEnded);
-        // require!(st.total_deposited >= st.min_raise_lamports, ErrorCode::MinRaiseNotMet);
+        require!(current_time >= st.funding_period_end, EngineErrorCode::FundingPeriodNotEnded);
+        require!(st.total_deposited >= st.min_raise_lamports, EngineErrorCode::MinRaiseNotMet);
         
         require!(st.vrf_seed.is_none(), EngineErrorCode::SeedAlreadySet);
 
