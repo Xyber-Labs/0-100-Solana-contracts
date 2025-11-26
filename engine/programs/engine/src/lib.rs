62c4f341dc5a469e845d0d309c1c497512fc15df
xykeeper <xykeeper@xyber.inc>
2025-11-26 14:14:41 +0500

    quick fixes

--- engine/programs/engine/src/lib.rs
+++ engine/programs/engine/src/lib.rs
@@ -20,7 +20,11 @@ mod instructions;
 mod state;
 pub mod utils;
 
+#[cfg(feature = "devnet")]
 declare_id!("DhKVzFTjzax7MeLEqiEXmEhm6ERSjehYaamqai5oPKZ7");
+
+#[cfg(not(feature = "devnet"))]
+declare_id!("xybxcJxiw7mp7SJvF9nRTB4ScGqShNXkxjuWdbuKRSn");
 
 #[program]
 pub mod engine {
