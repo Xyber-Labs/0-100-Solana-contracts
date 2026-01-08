a1d1f29fd21a10aa5197d26667297193ddbb26f1
xykeeper <xykeeper@xyber.inc>
2026-01-08 23:19:03 +0500

    refactor: polish state

--- engine/programs/engine/src/state.rs
+++ engine/programs/engine/src/state.rs
@@ -271,9 +271,9 @@ impl Reallocatable for Contribution {
 
 impl Contribution {
     pub fn total_tickets(&self) -> Result<u64> {
-        let count: u64 = 0;
+        let mut count: u64 = 0;
         for range in &self.ticket_ranges {
-            checked_add!(count, range.count())?;
+            count = checked_add!(count, range.count())?;
         }
         Ok(count)
     }
